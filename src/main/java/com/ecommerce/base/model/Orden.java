e9987e9fe27e10637381fa1490f4afdb4c9a379a
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-01-31 19:20:34 -0600

    Agregamos la clase para modelar a la orden de compra

---
+++ src/main/java/com/ecommerce/base/model/Orden.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ecommerce.base.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author nihil
+ */
+public class Orden {
+    private Integer id;
+    private String numero;
+    private Date fechaCreacion;
+    private Date fechaRecibida;
+    private double total;
+
+    public Orden() {
+    }
+
+    public Orden(Integer id, String numero, Date fechaCreacion, Date fechaRecibida, double total) {
+        this.id = id;
+        this.numero = numero;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaRecibida = fechaRecibida;
+        this.total = total;
+    }
+    
+    
+    
+    //Getters and Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaRecibida() {
+        return fechaRecibida;
+    }
+
+    public void setFechaRecibida(Date fechaRecibida) {
+        this.fechaRecibida = fechaRecibida;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "Orden{" + "id=" + id + ", numero=" + numero + ", fechaCreacion=" + fechaCreacion + ", fechaRecibida=" + fechaRecibida + ", total=" + total + '}';
+    }
+    
+}
