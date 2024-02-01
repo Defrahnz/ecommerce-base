d8ec6ae68c385138efa55cedf81b68cb7600c84f
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-01-31 18:42:04 -0600

    Commit inicial

---
+++ src/main/java/com/ecommerce/base/EcommerceGuitarcenterApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceGuitarcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceGuitarcenterApplication.class, args);
+	}
+
+}
