ec634188302e893329c0d5af774275581cb5dccb
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-02-02 01:13:52 -0600

    Agregamos los datos correspondientes para que se vinculen a las tablas de Usuario y DetalleOrden

--- src/main/java/com/ecommerce/base/model/Orden.java
+++ src/main/java/com/ecommerce/base/model/Orden.java
@@ -4,32 +4,51 @@
  */
 package com.ecommerce.base.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.util.Date;
 
 /**
  *
  * @author nihil
  */
+@Entity
+@Table(name="ordenes")
 public class Orden {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     private String numero;
     private Date fechaCreacion;
     private Date fechaRecibida;
     private double total;
+    
+    @ManyToOne
+    private User usuario;
+    
+    @OneToOne(mappedBy="orden")
+    private DetalleOrden detalle;
 
     public Orden() {
     }
 
-    public Orden(Integer id, String numero, Date fechaCreacion, Date fechaRecibida, double total) {
+    public Orden(Integer id, String numero, Date fechaCreacion, Date fechaRecibida, double total, User usuario, DetalleOrden detalle) {
         this.id = id;
         this.numero = numero;
         this.fechaCreacion = fechaCreacion;
         this.fechaRecibida = fechaRecibida;
         this.total = total;
+        this.usuario = usuario;
+        this.detalle = detalle;
     }
-    
-    
-    
+
+
+
     //Getters and Setters
 
     public Integer getId() {
@@ -70,6 +89,22 @@ public class Orden {
 
     public void setTotal(double total) {
         this.total = total;
+    }
+
+    public User getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(User usuario) {
+        this.usuario = usuario;
+    }
+
+    public DetalleOrden getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(DetalleOrden detalle) {
+        this.detalle = detalle;
     }
 
     @Override
