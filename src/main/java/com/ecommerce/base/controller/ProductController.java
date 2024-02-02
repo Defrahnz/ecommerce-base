189d33afcea2d9d88136fb140021e25349322495
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-02-02 15:59:44 -0600

    Se agregó el controller para redireccionar a productos

---
+++ src/main/java/com/ecommerce/base/controller/ProductController.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ecommerce.base.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author nihil
+ */
+@Controller
+@RequestMapping("/productos")
+public class ProductController {
+    
+    @GetMapping("")
+    public String show(){
+       return "products/show";
+   } 
+}
