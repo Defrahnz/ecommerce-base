cda04372b2fdf2137f439a87df4409c92e6c0a34
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-02-02 01:15:06 -0600

    Agregamos los datos correspondientes para que se vinculen a las tablas de Producto y Orden

--- src/main/java/com/ecommerce/base/model/DetalleOrden.java
+++ src/main/java/com/ecommerce/base/model/DetalleOrden.java
@@ -4,29 +4,52 @@
  */
 package com.ecommerce.base.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
 /**
  *
  * @author nihil
  */
+@Entity
+@Table(name="detalles")
 public class DetalleOrden {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private double cantidad;
     private double precio;
     private double total;
+    
+    @OneToOne
+    private Orden orden;
+    
+    @OneToOne
+    private Product producto;
+    
 
     public DetalleOrden() {
     }
 
-    public DetalleOrden(Integer id, String nombre, double cantidad, double precio, double total) {
+    public DetalleOrden(Integer id, String nombre, double cantidad, double precio, double total, Orden orden, Product producto) {
         this.id = id;
         this.nombre = nombre;
         this.cantidad = cantidad;
         this.precio = precio;
         this.total = total;
+        this.orden = orden;
+        this.producto = producto;
     }
-    
-    
+
+
+
+
+
     //Getters and Setters
 
     public Integer getId() {
@@ -69,6 +92,21 @@ public class DetalleOrden {
         this.total = total;
     }
 
+    public Orden getOrden() {
+        return orden;
+    }
+
+    public void setOrden(Orden orden) {
+        this.orden = orden;
+    }
+
+    public Product getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Product producto) {
+        this.producto = producto;
+    }
     @Override
     public String toString() {
         return "DetalleOrden{" + "id=" + id + ", nombre=" + nombre + ", cantidad=" + cantidad + ", precio=" + precio + ", total=" + total + '}';
