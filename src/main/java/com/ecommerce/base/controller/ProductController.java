a6e181e318044f2399a31a0bde2221ba644e7874
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-02-03 18:49:50 -0600

    Se le agregaron los cambios correspondientes para poder mostrar en la vista los productos agregados en la BD

--- src/main/java/com/ecommerce/base/controller/ProductController.java
+++ src/main/java/com/ecommerce/base/controller/ProductController.java
@@ -10,6 +10,7 @@ import com.ecommerce.base.service.ProductService;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +27,8 @@ public class ProductController {
     private ProductService productoService;
     
     @GetMapping("")
-    public String show(){
+    public String show(Model model){
+       model.addAttribute("productos", productoService.findAll());
        return "products/show";
    } 
     
