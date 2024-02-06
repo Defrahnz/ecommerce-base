3433e70daf28af2552001cef7040551387ab5ad9
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-02-06 15:32:08 -0600

    Se añadió el método que redirecciona a la vista del carrito

--- src/main/java/com/ecommerce/base/controller/HomeController.java
+++ src/main/java/com/ecommerce/base/controller/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -39,5 +40,10 @@ public class HomeController {
         producto=productoOpt.get();
         model.addAttribute("producto", producto);
         return "usuario/productohome";
+    }
+    @PostMapping("/cart")
+    public String addCart(){
+        
+        return "usuario/carrito";
     }
 }
