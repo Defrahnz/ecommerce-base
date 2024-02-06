c7d6b99409174b4ecaa4f6710bf9dacd12aceddf
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-02-05 22:41:09 -0600

    Agregamos un archivo de configuración para las rutas

---
+++ src/main/java/com/ecommerce/base/ResourceWebConfiguration.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ecommerce.base;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author nihil
+ */
+@Configuration
+public class ResourceWebConfiguration implements WebMvcConfigurer{
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations("file:images");
+    }
+    
+}
