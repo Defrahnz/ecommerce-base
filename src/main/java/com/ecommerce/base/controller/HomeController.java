beb8f1d29011d4b1b7d54d518629e0b898a82e79
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-02-06 15:00:57 -0600

    Agregamos una función para que redireccione a la vista del producto seleccionado

--- src/main/java/com/ecommerce/base/controller/HomeController.java
+++ src/main/java/com/ecommerce/base/controller/HomeController.java
@@ -5,10 +5,13 @@
 package com.ecommerce.base.controller;
 
 import com.ecommerce.base.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -18,11 +21,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    private final Logger logger=LoggerFactory.getLogger(HomeController.class);
     @Autowired
     private ProductService productoService;
     @GetMapping("")
     public String home(Model model){
         model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
+    }
+    @GetMapping("productohome/{id}")
+    public String productohome(@PathVariable Integer id){
+        logger.info("Id del producto enviado como parámetro: []",id);
+        
+        return "usuario/productohome";
     }
 }
