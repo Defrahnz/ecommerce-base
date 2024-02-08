b2d3cdd89ea9c04d2fdcd46fac8b5cf9c0bc7f84
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-02-08 14:51:06 -0600

    Se agregaron los archivos correspondientes para el manejo de usuarios desde la base de datos

---
+++ src/main/java/com/ecommerce/base/service/UserService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.ecommerce.base.service;
+
+import com.ecommerce.base.model.User;
+import java.util.Optional;
+
+/**
+ *
+ * @author nihil
+ */
+public interface UserService {
+    Optional<User> findById(Integer id);
+}
---
+++ src/main/java/com/ecommerce/base/service/UserServiceImp.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ecommerce.base.service;
+
+import com.ecommerce.base.model.User;
+import com.ecommerce.base.repository.UserRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author nihil
+ */
+@Service
+public class UserServiceImp implements UserService{
+    @Autowired
+    private UserRepository usuarioRepository;
+
+    @Override
+    public Optional<User> findById(Integer id) {
+        return usuarioRepository.findById(id);
+    }
+
+    
+}
