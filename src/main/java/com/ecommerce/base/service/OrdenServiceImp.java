71fdfea86af2a384ab22ab09e8d4a535b75f14a5
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-02-08 16:03:56 -0600

    Se implementó un método que retornará el findAll del service. Así mismo se implementó un método el cual generará el número de orden y le dará un formato necesario para guardarlo en la BD

--- src/main/java/com/ecommerce/base/service/OrdenServiceImp.java
+++ src/main/java/com/ecommerce/base/service/OrdenServiceImp.java
@@ -6,6 +6,8 @@ package com.ecommerce.base.service;
 
 import com.ecommerce.base.model.Orden;
 import com.ecommerce.base.repository.OrdenRepository;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +23,42 @@ public class OrdenServiceImp implements OrdenService{
     public Orden save(Orden orden) {
         return ordenRepository.save(orden);
     }
-    
+
+    @Override
+    public List<Orden> findAll() {
+        return ordenRepository.findAll();
+    }
+    public String generadorNumeroOrden(){
+        int num=0;
+        String numeroConcat="";
+        List<Orden> ordenes=findAll();
+        List<Integer> numeros=new ArrayList<>();
+        ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+        if(ordenes.isEmpty()){
+            num=1;
+        }else{
+            num=numeros.stream().max(Integer::compare).get();
+            num++;
+        }
+        if(num<10){
+            numeroConcat="000000000"+String.valueOf(num);
+        }else if(num<100){
+            numeroConcat="00000000"+String.valueOf(num);
+        }else if(num<1000){
+            numeroConcat="0000000"+String.valueOf(num);
+        }else if(num<10000){
+            numeroConcat="000000"+String.valueOf(num);
+        }else if(num<100000){
+            numeroConcat="00000"+String.valueOf(num);
+        }else if(num<1000000){
+            numeroConcat="0000"+String.valueOf(num);
+        }else if(num<10000000){
+            numeroConcat="000"+String.valueOf(num);
+        }else if(num<100000000){
+            numeroConcat="00"+String.valueOf(num);
+        }else if(num<1000000000){
+            numeroConcat="0"+String.valueOf(num);
+        }
+        return numeroConcat;
+    }
 }
