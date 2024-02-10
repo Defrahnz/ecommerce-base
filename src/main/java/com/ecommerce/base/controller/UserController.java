a821f7a04199d4d6c1f76cfec551a1500207ef85
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-02-10 01:45:49 -0600

    Se implementó el redireccionamiento del método "save" manda a llamar a lo demás y redirecciona a la vista principal

--- src/main/java/com/ecommerce/base/controller/UserController.java
+++ src/main/java/com/ecommerce/base/controller/UserController.java
@@ -4,10 +4,14 @@
  */
 package com.ecommerce.base.controller;
 
+import com.ecommerce.base.model.User;
 import com.ecommerce.base.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -17,11 +21,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/usuario")
 public class UserController {
+    private final Logger logger=LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService usuarioService;
     @GetMapping("/registro")
     public String create(){
         return "usuario/registro";
     }
-    
+    @PostMapping("/save")
+    public String save(User usuario){
+        logger.info("Usuario registro: {}",usuario);
+        usuario.setTipo("USER");
+        usuarioService.save(usuario);
+        return "redirect:/";
+    }
 }
