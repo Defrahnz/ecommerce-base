42314b39496c798579edf90c5a2a60f0525e4089
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-02-12 21:37:50 -0600

    Se agregó un método que redirecciona a la vista de compras

--- src/main/java/com/ecommerce/base/controller/UserController.java
+++ src/main/java/com/ecommerce/base/controller/UserController.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,5 +58,10 @@ public class UserController {
             logger.info("Usuario no existe");
         }
         return "redirect:/";
+    }
+    @GetMapping("/compras")
+    public String obtenerCompras(HttpSession sesion, Model model){
+        model.addAttribute("sesion",sesion.getAttribute("idusuario"));
+        return "usuario/compras";
     }
 }
